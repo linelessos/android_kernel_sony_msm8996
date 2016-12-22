0b5cd2b1ccf2e63403f689761c5cc10f96593a2a
Humberto Borba <humberos@gmail.com>
2016-12-22 09:12:30 -0200

    drivers: mmc: Introduce MMC_SD_DEFERRED_RESUME
    Add deferred MMC SD resume until runtime resume
    requested. This will reduce overall resume latency and
    save power when theres an SD card inserted but not being used
    
    Taken from Sony 39.2.A.0.361
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Signed-off-by: Erik Castricum <git@cas-online.nl>
    Change-Id: I4e151fc5be83ba076d0be5e505251ff269f5703e

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -4837,6 +4837,9 @@ static int mmc_pm_notify(struct notifier_block *notify_block,
 		notify_block, struct mmc_host, pm_notify);
 	unsigned long flags;
 	int err = 0, present = 0;
+#ifdef CONFIG_MMC_SD_DEFERRED_RESUME
+	bool pending_detect = false;
+#endif
 
 	switch (mode) {
 	case PM_RESTORE_PREPARE:
@@ -4847,7 +4850,15 @@ static int mmc_pm_notify(struct notifier_block *notify_block,
 		spin_lock_irqsave(&host->lock, flags);
 		host->rescan_disable = 1;
 		spin_unlock_irqrestore(&host->lock, flags);
+#ifdef CONFIG_MMC_SD_DEFERRED_RESUME
+		if (cancel_delayed_work_sync(&host->detect)) {
+			pending_detect = true;
+		}
+
+		mmc_cd_prepare_suspend(host, pending_detect);
+#else
 		cancel_delayed_work_sync(&host->detect);
+#endif
 
 		if (!host->bus_ops)
 			break;
@@ -4893,6 +4904,10 @@ static int mmc_pm_notify(struct notifier_block *notify_block,
 			break;
 		}
 		spin_unlock_irqrestore(&host->lock, flags);
+#ifdef CONFIG_MMC_SD_DEFERRED_RESUME
+		if (!mmc_cd_is_pending_detect(host))
+			break; /* IRQ should be triggered if CD changed */
+#endif
 		_mmc_detect_change(host, 0, false);
 
 	}
