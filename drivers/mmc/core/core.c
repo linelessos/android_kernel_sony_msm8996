176dfc654aaa991d04de86bc801aa6cbd945b830
Angelo G. Del Regno <kholk11@gmail.com>
2017-12-14 18:39:17 +0100

    mmc: core: Do not retune MMC after host clk ungate for Loire
    Either the SDHCI controller or the specific eMMC cards found
    in MSM8956 SoMC Loire devices do not support re-tuning:
    calling that without a full controller and card reset (which is
    expensive in terms of time and power) will produce the unability
    to get the card working in any HS mode.
    
    To work around that, just disable (hopefully temporarily) the
    call on Loire platform.

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -2455,6 +2455,7 @@ void mmc_ungate_clock(struct mmc_host *host)
 		WARN_ON(host->ios.clock);
 		/* This call will also set host->clk_gated to false */
 		__mmc_set_clock(host, host->clk_old);
+#ifndef CONFIG_ARCH_SONY_LOIRE
 		/*
 		 * We have seen that host controller's clock tuning circuit may
 		 * go out of sync if controller clocks are gated.
@@ -2462,6 +2463,7 @@ void mmc_ungate_clock(struct mmc_host *host)
 		 * tuning circuit after ungating the controller clocks.
 		 */
 		mmc_retune_needed(host);
+#endif
 	}
 }
 
