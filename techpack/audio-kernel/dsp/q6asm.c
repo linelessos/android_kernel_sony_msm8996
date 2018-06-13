6c44b0fb151a2710aa42666b07fb6d374d8a7ac5
Angelo G. Del Regno <kholk11@gmail.com>
2018-06-13 12:10:29 +0200

    techpack: audio: DSP: Force V3 functions for MSM8956 and MSM8996
    The drivers are sometimes calling the V4 functions directly for
    some monkey reason...
    This makes the DSP to go completely nuts on older platforms,
    since the firmware is not updated to support the V4 functions.
    
    To address this, even if - okay, it's ugly - add ifdefs to force
    calling the corresponding V3 functions: this makes sure that we
    won't get breakage from random CAF tags upgrades and such.
    
    Change-Id: I5fbe00131c28fd52d495d50fa7a87f4d0749a324

--- techpack/audio-kernel/dsp/q6asm.c
+++ techpack/audio-kernel/dsp/q6asm.c
@@ -2852,9 +2852,13 @@ EXPORT_SYMBOL(q6asm_open_read_v3);
 int q6asm_open_read_v4(struct audio_client *ac, uint32_t format,
 			uint16_t bits_per_sample, bool ts_mode)
 {
+#if defined(CONFIG_ARCH_MSM8916) || defined(CONFIG_ARCH_MSM8996)
+	return q6asm_open_read_v3(ac, format, bits_per_sample);
+#else
 	return __q6asm_open_read(ac, format, bits_per_sample,
 				 PCM_MEDIA_FORMAT_V4 /*media fmt block ver*/,
 				 ts_mode);
+#endif
 }
 EXPORT_SYMBOL(q6asm_open_read_v4);
 
@@ -3159,9 +3163,13 @@ EXPORT_SYMBOL(q6asm_open_write_v3);
 int q6asm_open_write_v4(struct audio_client *ac, uint32_t format,
 			uint16_t bits_per_sample)
 {
+#if defined(CONFIG_ARCH_MSM8916) || defined(CONFIG_ARCH_MSM8996)
+	return q6asm_open_write_v3(ac, format, bits_per_sample);
+#else
 	return __q6asm_open_write(ac, format, bits_per_sample,
 				  ac->stream_id, false /*gapless*/,
 				  PCM_MEDIA_FORMAT_V4 /*pcm_format_block_ver*/);
+#endif
 }
 EXPORT_SYMBOL(q6asm_open_write_v4);
 
@@ -3206,9 +3214,14 @@ int q6asm_stream_open_write_v4(struct audio_client *ac, uint32_t format,
 			       uint16_t bits_per_sample, int32_t stream_id,
 			       bool is_gapless_mode)
 {
+#if defined(CONFIG_ARCH_MSM8916) || defined(CONFIG_ARCH_MSM8996)
+	return q6asm_stream_open_write_v3(ac, format, bits_per_sample,
+						stream_id, is_gapless_mode);
+#else
 	return __q6asm_open_write(ac, format, bits_per_sample,
 				  stream_id, is_gapless_mode,
 				  PCM_MEDIA_FORMAT_V4 /*pcm_format_block_ver*/);
+#endif
 }
 EXPORT_SYMBOL(q6asm_stream_open_write_v4);
 
@@ -4252,6 +4265,12 @@ fail_cmd:
 		return rc;
 }
 
+int q6asm_enc_cfg_blk_pcm_v3(struct audio_client *ac,
+			     uint32_t rate, uint32_t channels,
+			     uint16_t bits_per_sample, bool use_default_chmap,
+			     bool use_back_flavor, u8 *channel_map,
+			     uint16_t sample_word_size);
+
 /*
  * q6asm_enc_cfg_blk_pcm_v4 - sends encoder configuration parameters
  *
@@ -4278,6 +4297,12 @@ int q6asm_enc_cfg_blk_pcm_v4(struct audio_client *ac,
 	u8 *channel_mapping;
 	u32 frames_per_buf = 0;
 	int rc;
+
+#if defined(CONFIG_ARCH_MSM8916) || defined(CONFIG_ARCH_MSM8996)
+	return q6asm_enc_cfg_blk_pcm_v3(ac, rate, channels, bits_per_sample,
+					use_default_chmap, use_back_flavor,
+					channel_map, sample_word_size);
+#endif
 
 	if (!use_default_chmap && (channel_map == NULL)) {
 		pr_err("%s: No valid chan map and can't use default\n",
@@ -5312,6 +5337,12 @@ static int __q6asm_media_format_block_pcm_v4(struct audio_client *ac,
 		pr_err("%s: Invalid channel count %d\n", __func__, channels);
 		return -EINVAL;
 	}
+#if defined(CONFIG_ARCH_MSM8916) || defined(CONFIG_ARCH_MSM8996)
+	return __q6asm_media_format_block_pcm_v3(ac, rate, channels,
+					bits_per_sample, stream_id,
+					use_default_chmap, channel_map,
+					sample_word_size);
+#endif
 
 	pr_debug("%s: session[%d]rate[%d]ch[%d]bps[%d]wordsize[%d]\n", __func__,
 		 ac->session, rate, channels,
@@ -5653,6 +5684,11 @@ static int __q6asm_media_format_block_multi_ch_pcm_v4(struct audio_client *ac,
 		pr_err("%s: Invalid channel count %d\n", __func__, channels);
 		return -EINVAL;
 	}
+#if defined(CONFIG_ARCH_MSM8916) || defined(CONFIG_ARCH_MSM8996)
+	return __q6asm_media_format_block_multi_ch_pcm_v3(ac, rate, channels,
+					use_default_chmap, channel_map,
+					bits_per_sample, sample_word_size);
+#endif
 
 	pr_debug("%s: session[%d]rate[%d]ch[%d]bps[%d]wordsize[%d]\n", __func__,
 		 ac->session, rate, channels,
