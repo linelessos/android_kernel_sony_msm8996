812fa739cb15416fbb08e2630493a27e7e724d45
Erik Castricum <git@cas-online.nl>
2019-03-27 13:17:56 +0100

    tone: sony sound (41.3.A.2.192)
    Signed-off-by: Erik Castricum <git@cas-online.nl>
    Change-Id: I6a313037fd645034b8e79d84f86e6a2e0b936c7f

--- sound/soc/msm/qdsp6v2/msm-lsm-client.c
+++ sound/soc/msm/qdsp6v2/msm-lsm-client.c
@@ -1551,7 +1551,6 @@ static int msm_lsm_ioctl_compat(struct snd_pcm_substream *substream,
 			err = -EINVAL;
 			goto done;
 		}
-
 		if (copy_from_user(&p_data_32, arg,
 				   sizeof(p_data_32))) {
 			dev_err(rtd->dev,
@@ -1760,7 +1759,6 @@ static int msm_lsm_ioctl(struct snd_pcm_substream *substream,
 			err = -EINVAL;
 			goto done;
 		}
-
 		if (copy_from_user(&p_data, arg,
 				   sizeof(p_data))) {
 			dev_err(rtd->dev,
