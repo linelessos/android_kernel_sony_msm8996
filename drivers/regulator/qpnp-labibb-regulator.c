f86c3773b3703085bdb3b403501229f16c41a52e
Erik Castricum <git@cas-online.nl>
2019-09-19 13:30:38 +0200

    tone: fix labibb regulator
    Signed-off-by: Erik Castricum <git@cas-online.nl>
    Change-Id: Icfd71121aa6f21143f41047a5907dc2f043bc87f

--- drivers/regulator/qpnp-labibb-regulator.c
+++ drivers/regulator/qpnp-labibb-regulator.c
@@ -3854,7 +3854,7 @@ static void vreg_check_worker(struct work_struct *work)
 		goto read_error;
 	}
 
-	if (!(val & LAB_STATUS1_VREG_OK))
+	if (!(val & LAB_STATUS1_VREG_OK_BIT))
 		goto status_error;
 
 	/* ibb vreg status check */
@@ -3866,7 +3866,7 @@ static void vreg_check_worker(struct work_struct *work)
 		goto read_error;
 	}
 
-	if (!(val & IBB_STATUS1_VREG_OK))
+	if (!(val & IBB_STATUS1_VREG_OK_BIT))
 		goto status_error;
 
 	rc = qpnp_labibb_interrupt_enable_ctl(labibb, VREG_WORKER_ACTIVE);
@@ -4964,7 +4964,7 @@ static irqreturn_t lab_vreg_handler(int irq, void *_chip)
 		goto exit;
 	}
 
-	if (!(val & LAB_STATUS1_VREG_OK))
+	if (!(val & LAB_STATUS1_VREG_OK_BIT))
 		pr_err("%s: LAB VREG NG!!!\n", __func__);
 	else
 		goto false_detection;
@@ -5012,7 +5012,7 @@ static irqreturn_t ibb_vreg_handler(int irq, void *_chip)
 		goto exit;
 	}
 
-	if (!(val & IBB_STATUS1_VREG_OK))
+	if (!(val & IBB_STATUS1_VREG_OK_BIT))
 		pr_err("%s: IBB VREG NG!!!\n", __func__);
 	else
 		goto false_detection;
