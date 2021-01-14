sonyxperiadev/kernel

msm: mdss: Add somc_panel In-Cell support code
Add somc_panel In-Cell support code to generic QC framework.
While at it, fix a small change in hdmi_tx for better MHL
support.

include/linux/incell.h
@@ -1,0 +1,1 @@
+#include <../../drivers/video/msm/mdss/somc_panel/incell.h>

linelessos/android_kernel_sony_msm8996

include/linux/incell.h
@@ -1,0 +1,1 @@
+#include <../../drivers/video/fbdev/msm/somc_panel/incell.h>